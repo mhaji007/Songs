03d21f38b1583029f9a1d2b1906c3b62d5ad6b9e
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-03-12 12:09:57 -0400

    Add connect function

---
+++ src/components/SongList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+
+class SongList extends React.Component {
+    render() {
+        return (
+            <div>
+                SongList
+            </div>
+        )
+    }
+}
+
+export default connect() (SongList);
+
+
