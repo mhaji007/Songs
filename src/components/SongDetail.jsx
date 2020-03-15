4ad07f788129a8ae15bbe9bc6dc5ba5eabae2640
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-03-14 20:32:03 -0400

    Add conditional rendering

--- src/components/SongDetail.jsx
+++ src/components/SongDetail.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const SongDetail = (props) => {
-    return <div>Song Detail</div>
+const SongDetail = ({song}) => {
+if (!song) {
+    return <div>Select a song </div>
+}
+return (
+<div>
+    <h3>Details for: </h3>
+    <p>Title:  {song.title} </p>
+    <p>Duration:  {song.duration} </p>
+</div>
+);
 };
 
 const mapStateToProps = (state) => {
