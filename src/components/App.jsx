73dff1f4b66ba5ffa3456b537f57dcece255069b
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-03-14 20:10:15 -0400

    Wiring SongDetail component with connect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
     return (
     <div className="ui container grid">
         <div className="ui row">
-            <div className="column eigth wide">
+            <div className="eight wide column">
             <SongList/>
             </div>
-    
+            
+             <div className="eight wide column">
+            <SongDetail/>
+            </div>
         </div>    
     </div>
     );
